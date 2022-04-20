67d73c5a1b2ed507ce54ea91e6a4ef3ec11cb903
am-cassini <azaleamae.manimog@msugensan.edu.ph>
2022-04-20 22:17:55 +0800

    revert files

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -29,6 +29,31 @@ $(function () {
       fill: false
     }]
   };
+  var data_dashboard = {
+    labels: ["Year 1", "Year 2", "Year 3", "Year 4 (Pending)"],
+    datasets: [{
+      label: 'CGPA',
+      data: [3.3, 3.7, 4.0, 3.7],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      fill: false
+    }]
+  };
   var dataDark = {
     labels: ["1st | 2020-2021", "2nd | 2020-2021", "1st | 2021-2022", "2nd | 2021-2022"],
     datasets: [{
@@ -459,6 +484,15 @@ $(function () {
     var barChart = new Chart(barChartCanvas, {
       type: 'bar',
       data: data,
+      options: options
+    });
+  }
+  if ($("#barChart_dashboard").length) {
+    var barChartCanvas = $("#barChart_dashboard").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart_dashboard = new Chart(barChartCanvas, {
+      type: 'bar',
+      data: data_dashboard,
       options: options
     });
   }
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,4 +1,4 @@
-(function($) {
+git (function($) {
   'use strict';
   $(function() {
 
